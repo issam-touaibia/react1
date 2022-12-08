555c0494fa2fe92ee89c45581b922591470fbf68
issam-touaibia <issamtouaibia1234@gmail.com>
2022-12-08 15:16:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React, { Profiler } from 'react'
-//import './App.css';
-import FullName from './profile/FullName';
-import Address from './profile/Address';
-import ProfilePhoto from './profile/ProfilePhoto';
-import './profile/Formilaire.css'
-
-function App() {
-  return (
-  <div className='issam'>
-
-<FullName/>
-<Address/>
-<ProfilePhoto/>
-
-
-
-
-
-</div>
-
-)
-}
+import Button from 'react-bootstrap/Button';
+import './App.css'
+import  React,{Component}  from "react";
+import img from './Profile/pic.jpg'
+import Profile from './Profile/Profile';
+  class App extends React.Component{
     
+    constructor(props){
+      
+    super(props) 
+    
+   
+this.state={person:{
+  fullName:'Issam Touaibia',
+  profession:"Web development",
+  bio:" I am a kung fu instructor who love the sport since  I was young and want to progress  in this field for the better via web devoloppment  " ,
+  Photo:< img src={img} alt="image"/>
+
+  },
+   
+  show: false
   
-export default App
+    }
+   
+ }
+ 
+
+render(){
+
 
   
+return(
+ 
+  <div className='app-header'>
+  {
+    this.state.show?
+      <div className=''>
+        <h1 className='fullName'>{this.state.person.fullName}</h1>
+        <h2 className='bio'>{this.state.person.bio}</h2>
+        <h1 className='profession'>{this.state.person.profession}</h1>
+        <div className='Photo'>{ this.state.person.Photo}</div>
+        <Profile>
+       </Profile>
+      </div> 
+    :null
+      
+         
+    
+  }
+    
+   <Button variant="success" onClick={()=> this.setState({...this.state,show:!this.state.show})} >Click Me</Button>
+  </div>
 
+);
+ 
+}
+ 
+}
 
-
-  
+export default App;
